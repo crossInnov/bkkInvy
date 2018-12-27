71ef58dcf8459170f1e86703173c965d3a9d6e25
akrich12 <akrich.1205@gmail.com>
2018-12-27 19:42:19 +0100

    can you see the Visualstudio's project?

---
+++ app27122018/app27122018/Controllers/EmpleadosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using app27122018.Models;
+
+namespace app27122018.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private PresenciaEmpleadosEntities db = new PresenciaEmpleadosEntities();
+
+        // GET: Empleados
+        public ActionResult Index()
+        {
+            var empleados = db.Empleados.Include(e => e.Jornadas);
+            return View(empleados.ToList());
+        }
+
+        // GET: Empleados/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // GET: Empleados/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmpleadoID = new SelectList(db.Jornadas, "JornadaID", "Horario");
+            return View();
+        }
+
+        // POST: Empleados/Create
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmpleadoID,Apelido,Nombre,Puesto,Loguin,contrasena")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Empleados.Add(empleados);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmpleadoID = new SelectList(db.Jornadas, "JornadaID", "Horario", empleados.EmpleadoID);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmpleadoID = new SelectList(db.Jornadas, "JornadaID", "Horario", empleados.EmpleadoID);
+            return View(empleados);
+        }
+
+        // POST: Empleados/Edit/5
+        // Afin de déjouer les attaques par sur-validation, activez les propriétés spécifiques que vous voulez lier. Pour 
+        // plus de détails, voir  https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmpleadoID,Apelido,Nombre,Puesto,Loguin,contrasena")] Empleados empleados)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(empleados).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmpleadoID = new SelectList(db.Jornadas, "JornadaID", "Horario", empleados.EmpleadoID);
+            return View(empleados);
+        }
+
+        // GET: Empleados/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Empleados empleados = db.Empleados.Find(id);
+            if (empleados == null)
+            {
+                return HttpNotFound();
+            }
+            return View(empleados);
+        }
+
+        // POST: Empleados/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Empleados empleados = db.Empleados.Find(id);
+            db.Empleados.Remove(empleados);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
